19ffc49322711d528c0ecdee0a90c58eee3f794d
irshad_aj <irshadaj08@gmail.com>
2022-10-26 18:27:39 +0530

    Req tag tried but not working

--- ASPNETMVCCRUD/Models/AddEmployeeViewModel.cs
+++ ASPNETMVCCRUD/Models/AddEmployeeViewModel.cs
@@ -11,7 +11,7 @@ namespace ASPNETMVCCRUD.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Department { get; set; }
-        [Required]
+        
         public long Salary { get; set; }
         public DateTime DateOfBirth { get; set; }
     }
